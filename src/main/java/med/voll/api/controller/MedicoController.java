d4ac47bd5ce63aa2e7b1d2d93c76496c7eca2bb2
Caio Couto <caiocc20006@gmail.com>
2026-04-27 20:36:13 -0300

    Adiciona metodo Get

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,15 +2,16 @@ package med.voll.api.controller;
 
 
 import jakarta.validation.Valid;
+import med.voll.api.medicos.DadosListagemMedicos;
 import med.voll.api.medicos.DadosMedicos;
 import med.voll.api.medicos.Medico;
 import med.voll.api.medicos.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/medicos")
@@ -23,5 +24,10 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosMedicos dadosMedicos){
         repository.save(new Medico(dadosMedicos));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedicos> listar(Pageable pageable){
+        return repository.findAll(pageable).map(DadosListagemMedicos::new);
     }
 }
--- src/main/java/med/voll/api/pacientes/DadosPacientes.java
+++ src/main/java/med/voll/api/pacientes/DadosPacientes.java
@@ -17,7 +17,7 @@ public record DadosPacientes(
         String email,
 
         @NotBlank
-        @Pattern(regexp = "\\d{11}")
+        @Pattern(regexp = "^\\d{3}\\.?\\d{3}\\.?\\d{3}-?\\d{2}$")
         String cpf,
 
         @NotBlank
